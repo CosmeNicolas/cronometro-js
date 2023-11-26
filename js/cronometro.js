b61397b048effb1fb7bcbb7101c95352619cceae
CosmeNicolas <nicomas7590@gmail.com>
2023-11-26 18:24:26 -0300

    agregue las funciones pausar y resetear con clear interval para volver al numero de inicio 0

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,31 +1,44 @@
 /* 5- Realizar una web con un cronómetro, que tenga las opciones de iniciar, reset (volver el cronómetro a 0) y pausar.
  */
 
-let tiempoDeEspera;
-let tiempoDeInicio = false;
-let hora = 0
-let minuto = 0
-let segundo = 0 
+let tiempo;
+let horas = 0
+let minutos = 0
+let segundos = 0 
 
-const iniciar = ()=>{
-  console.log('inicia el cronometro')
-  if(!tiempoDeInicio ){
-    tiempoDeInicio = true;
-    tiempoDeEspera = setInterval(actualizarTiempo, 1000)
+const inicio = ()=>{
+    tiempo = setInterval(actualizarTiempo,1000);
+    console.log(tiempo)
   }
+
+const pausar = ()=>{
+  clearInterval(tiempo);
 }
 
+const resetear = () =>{
+  clearInterval(tiempo);
+  segundos = 0;
+  minutos = 0;
+  horas = 0;
+    
+}
+
+
 const actualizarTiempo = () =>{
-  segundo++
- // console.log(segundo);
-  if(segundo>=60){
-    segundo = 0;
-    minuto++;
+  segundos++
+ console.log(segundos);
+  if(segundos>=60){
+    segundos = 0;
+    minutos++;
     console.log(minuto);
-    if(minuto >= 60){
-      minuto = 0;
-      hora++;
+    if(minutos >= 60){
+      minutos = 0;
+      horas++;
     }
   }
 }
+
+
+
+//document.getElementById('tiempo').addEventListener('click', iniciar)
 
